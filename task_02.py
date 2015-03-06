3ecda10c9e11c75664ab46fa1658a81c4f56d606
gpsingh12 <gurpreet_1441@yahoo.com>
2015-03-06 01:14:15 +0000

    Time

---
+++ task_02.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Module imports peanut from task_01
+    and assigns a value to its constant
+    from peanut"""
+
+import task_01.peanut
+TIME = task_01.peanut.BUTTER
