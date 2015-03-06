f61b1214b63738dfacfc0a6a930150ab9098f745
gpsingh12 <gurpreet_1441@yahoo.com>
2015-03-06 01:13:16 +0000

    Jelly

---
+++ task_03.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Module 3 copies the constant BUTTER
+   from peanut and assign its value to
+   a new constant JELLY"""
+
+from task_01.peanut import BUTTER
+JELLY = BUTTER
